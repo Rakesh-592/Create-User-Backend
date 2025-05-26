2cbe4d1ead23b86f8a12fa326697d2a8ba08681e
Rakesh-592 <rakeshkunapareddy333@gmail.com>
2025-05-26 10:05:22 +0530

    updated code

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/myapp");
+// mongoose.connect("mongodb://127.0.0.1:27017/myapp");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const userSchema = mongoose.Schema({
   name: String,
